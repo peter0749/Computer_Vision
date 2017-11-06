6c7267903a35694d931b3e2f2222b150b0e7c4af
jengku <jengku@gmail.com>
2017-11-06 15:02:33 +0800

    foo

--- hw1_Gaussian_Filter/my_conv.m
+++ hw1_Gaussian_Filter/my_conv.m
@@ -5,10 +5,13 @@
 % And z is actually: z = x*y (x convolution y)
 % As for padding size: 
 %   see CS231n slides: http://cs231n.github.io/convolutional-networks/
-function Y = my_conv(X,kernel)
+function Y = my_conv(X,kernel,padding)
+    if nargin < 3
+        padding = 'replicate';
+    end
     kernel_size = floor((size(kernel)-1)./2); % compute padding size
     X_size = size(X); % original image size
-    X = padarray(X, [kernel_size(1) kernel_size(2)], 'replicate', 'both'); % padding img
+    X = padarray(X, [kernel_size(1) kernel_size(2)], padding, 'both'); % padding img
     fftX = fft2(double(X));
     fftk = fft2(padarray(double(kernel), size(X)-size(kernel), 0, 'pre'));
     Y = real(ifft2( fftX.*fftk ));
