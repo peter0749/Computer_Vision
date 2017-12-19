dcc79c614f937579cd392c34dd715216fb37c550
jengku <jengku@gmail.com>
2017-12-19 12:47:00 +0800

    foo

--- hw3_2d_3d/hw3.m
+++ hw3_2d_3d/hw3.m
@@ -13,15 +13,15 @@ A = [[d3 zeros(records,4) -d2(:,1).*d3(:,1) -d2(:,1).*d3(:,2) -d2(:,1).*d3(:,3) 
     ]; % Matrix A
 [V , lambda] = eig(A' * A); % (t(A)*A)*v = lambda*v
 [min_eig_val, idx] = min(sum(lambda)); % find argmin_eigvalue(eigvector)
-P = reshape(V(:,idx), 4,3)'; % get Projection Matrix ###
+P = reshape(V(:,idx), 4,3)' % get Projection Matrix ###
 
 M = P(:,1:3);
 % invM = QR -> K=inv(R); Rot=inv(Q)
 [Q, R] = qr(inv(M));
-K=inv(R); % get Calibration Matrix ###
-Rot=inv(Q); % get Rotation Matrix ###
-T=K\P(:,4); % get Translation Matrix ###
+K=inv(R) % get Calibration Matrix ###
+Rot=inv(Q) % get Rotation Matrix ###
+T=K\P(:,4) % get Translation Matrix ###
 
 projectedD2 = (P*d3')';
 projectedD2 = projectedD2./projectedD2(:,3); % convert from homogeneous coordinate
-projection_error = mean(sqrt(sum((d2(:,1:2)-projectedD2(:,1:2)).^2,2))); % report projection error ### 
+projection_error = mean(sqrt(sum((d2(:,1:2)-projectedD2(:,1:2)).^2,2))) % report projection error ### 
